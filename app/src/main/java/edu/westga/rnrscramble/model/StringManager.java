94f86d8315759439478c3cfcf20cbe73dbc64dee
Ron Struempf <rstruem1@my.westga.edu>
2016-03-25 22:17:04 -0400

    Created framework for the code I will add.  Preparing to add unit tests

---
+++ app/src/main/java/edu/westga/rnrscramble/model/StringManager.java
@@ -0,0 +1,33 @@
+package edu.westga.rnrscramble.model;
+
+/**
+ * Utility code to make changes to character strings
+ *
+ * Created by Ron on 3/25/2016.
+ */
+public class StringManager {
+    /**
+     * See whether a string contains a given character
+     *
+     * @param str String to search
+     * @param chr Character to search for
+     * @return True if the string contains the given character
+     */
+    public static boolean contains(String str, char chr) {
+        // TODO: Finish
+        return false;
+    }
+
+    /**
+     * Remove the first occurrence, if any, of a given character from a string
+     *
+     * @param str String to modify
+     * @param chr Character to remove
+     * @return Modified string
+     */
+    public static String remove(String str, char chr)
+    {
+        // TODO: Finish
+        return str;
+    }
+}
